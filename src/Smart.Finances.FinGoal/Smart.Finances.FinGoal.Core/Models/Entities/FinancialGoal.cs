abd0e282ba0c9ecece94f38afc6e1ef862ceb020
eglison.souza2019 <eglison.souza2019@gmail.com>
2024-02-05 20:53:08 -0300

    feat: add property type

--- src/Smart.Finances.FinGoal/Smart.Finances.FinGoal.Core/Models/Entities/FinancialGoal.cs
+++ src/Smart.Finances.FinGoal/Smart.Finances.FinGoal.Core/Models/Entities/FinancialGoal.cs
@@ -1,12 +1,15 @@
 ﻿using Smart.Finances.FinGoal.Core.Models.Enuns;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Smart.Finances.FinGoal.Core.Models.Entities
 {
     public class FinancialGoal : BaseEntity
     {
         public string Name { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
         public decimal GoalAmount { get; set; }
         public DateTime? Deadline { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
         public Decimal? IdealMonthySaving { get; set; }
         public FinancialGoalStatus Status { get; set; }
         public IList<FinancialGoalTransactions>? Transactions { get; set; }
--- src/Smart.Finances.FinGoal/Smart.Finances.FinGoal.Core/Models/Entities/FinancialGoalTransactions.cs
+++ src/Smart.Finances.FinGoal/Smart.Finances.FinGoal.Core/Models/Entities/FinancialGoalTransactions.cs
@@ -1,11 +1,15 @@
 ﻿using Smart.Finances.FinGoal.Core.Models.Enuns;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Smart.Finances.FinGoal.Core.Models.Entities
 {
     public class FinancialGoalTransactions : BaseEntity
     {
+        [Column(TypeName = "decimal(18,4)")]
         public decimal Amount { get; set; }
         public DateTime TransactionDate { get; set; }
         public TransactionType TransactionType { get; set; }
+        public FinancialGoal FinancialGoal { get; set; }
+        public Guid FinancialGoalId { get; set; }
     }
 }
