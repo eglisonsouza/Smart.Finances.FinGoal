82c317427f0b0bc4bd516fbdc938c34446ea79ca
eglison.souza2019 <eglison.souza2019@gmail.com>
2024-02-05 19:54:15 -0300

    chore: add config EfCore

---
+++ src/Smart.Finances.FinGoal/Smart.Finances.FinGoal.Infra/Persistence/Configuration/SqlServerConfig.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Smart.Finances.FinGoal.Core.Models.Entities;
+
+namespace Smart.Finances.FinGoal.Infra.Persistence.Configuration
+{
+    public class SqlServerConfig
+    {
+        public DbSet<FinancialGoal> FinancialGoals { get; set; }
+        public DbSet<FinancialGoalTransactions> FinancialGoalTransactions { get; set; }
+    }
+}
